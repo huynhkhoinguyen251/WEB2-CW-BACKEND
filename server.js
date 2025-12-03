9dbeffc72079ac6899a0c3e520b4077c4e8c9414
huynhkhoinguyen251 <emailken098@gmail.com>
2025-12-03 19:02:31 +0700

    a

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-
-// global.Vocab = require('./api/models/vocabModel');
-// const routes = require('./api/routes/vocabRoutes');
-
-// mongoose.set('strictQuery', true);
-// mongoose.connect(
-//   'mongodb+srv://nguyenhkgcs230231_db_user:5VLFIMxct8zwmoJM@cluster1.u3k4vks.mongodb.net/?appName=Cluster1'
-// );
-
-// const port = process.env.PORT || 3000;
-// const app = express();
-
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// routes(app);
-// app.listen(port);
-
-// app.use((req, res) => {
-//   res.status(404).send({ url: `${req.originalUrl} not found` });
-// });
-
-// console.log(`Server started on port ${port}`);
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
